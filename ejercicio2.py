5f3175e4c2bdaebd9db2f95a34a2e4ac03ac6474
sharith-murillo <sharithmurillo65@gmail.com>
2024-04-18 11:20:42 -0500

    hola

---
+++ ejercicio2.py
@@ -0,0 +1,13 @@
+a=int(input("digite el valor para a:"))
+b=int(input("digite el valor para b:"))
+suma=a+b
+print("la suma es:", suma)
+
+a=int(input("digite el valor para a:"))
+c=int(input("digite le valor para c:"))
+resta=a-c
+print("la resta es:",resta)
+
+d=int(input("digite el valor para d"))
+s=int(input("digite el valor para s"))
+musltiplicacion= d*s
