3f45a52b59f5522db7a052b6fe488bd5f8ff9037
sharith-murillo <sharithmurillo65@gmail.com>
2024-04-18 10:28:01 -0500

    commit 1!

---
+++ ejercicio1.py
@@ -0,0 +1,24 @@
+a=3
+b=1
+suma= a+b
+print("la suma es", suma)
+
+a=33
+c=20
+resta= a-c
+print ("la resta es", resta)
+
+d= 100
+s= 70
+multiplicacion= d*s
+print("la operacio es", multiplicacion)
+
+f=44
+g=15
+cociente= f/g
+print("el resultado es", ciciente)
+
+t=19
+h= 90
+residuo= t%h
+print("el resultado es", residuo)
