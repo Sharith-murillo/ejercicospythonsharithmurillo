879e976eb076edcf7fd054c242ad6b89ff04b6bf
sharith-murillo <sharithmurillo65@gmail.com>
2024-04-23 08:19:56 -0500

    mi primer commit

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,7 +1,7 @@
 a=int(input("digite el valor para a:"))
 b=int(input("digite el valor para b:"))
 suma=a+b
-print("la suma es:", suma)
+print("la suma es:",suma)
 
 a=int(input("digite el valor para a:"))
 c=int(input("digite le valor para c:"))
@@ -10,4 +10,16 @@ print("la resta es:",resta)
 
 d=int(input("digite el valor para d"))
 s=int(input("digite el valor para s"))
-musltiplicacion= d*s
+multiplicacion= d*s
+print("el resultado es:",multiplicacion)
+
+f=int(input("digite el valor para f:"))
+g=int(input("digie el valor para g:"))
+cociente= f/g
+print("el resultado es:",cociente)
+
+t=int(input("digite el valor para t:")) 
+h=int(input("digite el valor para h:"))
+residuo= t%h
+print("el resultado es:",residuo)
+
---
+++ ejercicio3.py
@@ -0,0 +1,5 @@
+compra=int(input("digite el valor de la compra:"))
+billete=int(input("digite el valor del billete:"))
+
+cambio=compra-billete
+print("el cambio seria:",cambio)
